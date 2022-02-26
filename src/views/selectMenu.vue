05d21f65b309b1a5859073e8dd83a858f4273bde
koshiko19 <a.koshiko@leihauoli.com>
2022-02-26 15:47:23 +0900

    メニューデータをjsonで管理するよう修正

--- src/views/selectMenu.vue
+++ src/views/selectMenu.vue
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="select-choice-wrap">
 			<h2 class="choice-title">今日のご飯は</h2>
-			<img class="select-menu-image" :src="image">
+			<img class="select-menu-image" :src="require('@/assets/img/' + image)">
 			<div class="select-menu">{{ title }}</div>
 			<div class="select-button">
 				<router-link to="/">選びなおす！</router-link>
@@ -12,44 +12,14 @@
 </template>
 
 <script>
+	import menu from '../assets/menuData.json'
 	export default {
 		data () {
 			return {
+				detailData: menu,
 				id: "",
 				title: "",
 				image: "",
-				detailData: [
-					[
-						{ id: 1, title: '焼き魚', image: require('@/assets/img/yakisakana.jpg') },
-						{ id: 1, title: 'うどん', image: require('@/assets/img/udon.jpg') },
-						{ id: 1, title: '寿司', image: require('@/assets/img/sushi.jpg') },
-					],
-					[
-						{ id: 2, title: 'ハンバーグ', image: require('@/assets/img/hamburger.jpg') },
-						{ id: 2, title: 'オムライス', image: require('@/assets/img/omuraisu.jpg') },
-						{ id: 2, title: 'カレーライス', image: require('@/assets/img/curry.jpg') },
-					],
-					[
-						{ id: 3, title: 'ピザ', image: require('@/assets/img/pizza.jpg') },
-						{ id: 3, title: 'スパゲッティ', image: require('@/assets/img/pasta.jpg') },
-						{ id: 3, title: 'フレンチ', image: require('@/assets/img/pasta.jpg') },
-					],
-					[
-						{ id: 4, title: '餃子', image: require('@/assets/img/gyouza.jpg') },
-						{ id: 4, title: 'ラーメン', image: require('@/assets/img/ramen.jpg') },
-						{ id: 4, title: '肉まん', image: require('@/assets/img/nikuman.jpg') },
-					],
-					[
-						{ id: 5, title: 'チゲ鍋', image: require('@/assets/img/tigenabe.jpg') },
-						{ id: 5, title: '鍋焼きうどん', image: require('@/assets/img/nabeyaki.jpg') },
-						{ id: 5, title: 'すきやき', image: require('@/assets/img/sukiyaki.jpg') },
-					],
-					[
-						{ id: 6, title: 'サンドイッチ', image: require('@/assets/img/sandwich.jpg') },
-						{ id: 6, title: 'パン', image: require('@/assets/img/pan.jpg') },
-						{ id: 6, title: 'おにぎり', image: require('@/assets/img/onigiri.jpg') },
-					]
-				],
 			}
 		},
 		created () {
